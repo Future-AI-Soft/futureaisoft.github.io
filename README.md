18041ec8fbe285ee86881c5adb9b6e004ce84d77
Your Name <you@example.com>
2022-01-03 12:11:42 +0600

    update website

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+https://preview.themeforest.net/item/landrick-responsive-saas-and-software-template/full_screen_preview/24438577?_ga=2.66419246.1218006422.1641183894-649664818.1641183894
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
