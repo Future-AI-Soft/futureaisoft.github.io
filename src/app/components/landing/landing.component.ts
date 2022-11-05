a7e8c61d253d7148148964046d300a4e537962a4
Farid Ahmed <w3farid@gmail.com>
2022-11-05 23:44:26 +0600

    config seo

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit { 
 
-  constructor() { }
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      {name: 'description', content: 'Home page'},
+      {name: 'author', content: 'FutureAI Software Development Services Ltd.'},
+      {name: 'keywords', content: 'Custom Software, Mobile Application, DevOps'}
+    ]);
+    this.title.setTitle('Home Page')
+   }
+
+  
 
   ngOnInit(): void {
   }
