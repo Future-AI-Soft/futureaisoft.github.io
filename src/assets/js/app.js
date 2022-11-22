fac81039734d0ee5c0fa9dcfa5367784a1d48910
Towru Khan <khantowru@gmail.com>
2022-11-22 16:50:58 +0600

    Logo chang with footer text & logo add

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
-/* Template Name: Landrick - Saas & Software Landing Page Template
-   Author: Shreethemes
-   E-mail: support@shreethemes.in
+/* Template Name: futureaisoft - Saas & Software Landing Page Template
+   Author: futureaisoft
+   E-mail: support@futureaisoft.com
    Created: August 2019
    Version: 3.8.0
    Updated: July 2021
